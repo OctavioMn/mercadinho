6b3d37965120c703c02f74a67a2a32e00813d20a
Juan Pablo <jpprog1@gmail.com>
2023-01-11 22:13:00 -0300

    Tela de Login

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
   #get 'devise/session#new'
-  root to: 'home#index'
+  #devise_for :users
   devise_for :users
+
+  devise_scope :user do  
+    get '/users/sign_out' => 'devise/sessions#destroy'     
+  end
+
   resources :produtos
   resources :vendas
   resources :clientes
+
+  root to: 'home#index'
 end
